46670fb0440309ccd3d4264ac560890976f89e86
hkobr <hassou92@gmail.com>
2015-07-07 23:12:22 +0200

    CPA EGREP

---
+++ LexParse/Compilation/comp.bat
@@ -0,0 +1,4 @@
+jflex-1.6.0\bin\jflex.bat ..\TME4.l
+yacc.exe -J ..\TME4.y
+
+PAUSE
---
+++ LexParse/Compilation/comp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./jflex-1.6.0/bin/jflex ../TME4.l
+./byacc -J -Jthrows ../TME4.y
+
+echo "comp.sh ....... done"
